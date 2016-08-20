e0249624c59da1ca9808c47dddc0ab5825bfd29a
Alex Wyrick <alexanderwyrick@gmail.com>
2016-08-20 14:56:04 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('it works');
+app.getWeather = function(){
+	var cityId = $( this ).val();
+		$.ajax({
+			url: 'https://api.edx.org/api/courses/v1/courses/2YotnFZFEjr1zCsicMWpAA',
+			success: function( response ){
+				var temp = (Math.floor(response.main.temp - 273.15)*9/5)+32 ;
+				$( '#response' ).text( '°' + temp + ' ' + 'and ' +  '  ' + sky + '  ' + 'wind speed = ' + wind);
+				/* $( '#response' ).text( sky ); */
+
+		}
+	});
+};
+
+/*
+{
+       "access_token":"2YotnFZFEjr1zCsicMWpAA",
+       "token_type":"example",
+       "expires_in":3600,
+       "refresh_token":"tGzv3JOkF0XG5Qx2TlKWIA",
+       "example_parameter":"example_value"
+     }
+*/
